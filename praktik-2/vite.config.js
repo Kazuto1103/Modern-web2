0d8a18c09466b1f81c5836a140d0ece17c4546af
Kazuto1103 <adamsyachpranawalidin@gmail.com>
2025-12-16 09:03:38 +0700

    naming mistake path vite 2

--- praktik-2/vite.config.js
+++ praktik-2/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Pastikan tulisan 'praktik-4' SAMA PERSIS dengan nama repo di URL GitHub
 export default defineConfig({
   plugins: [react()],
-  base: "/praktik-2/", 
+  base: './'
 })
